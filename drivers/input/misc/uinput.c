035b13c853148ea968d67b5ef8369795d9fcc1a3
LukasAddon <LukasAddon@gmail.com>
2016-04-19 22:16:35 +0300

    Add a hack to throttle orientation sensor input events
    The userspace orientation sensor code in Android ICS seems to constantly
    send updates through the uinput subsystem, which causes the CPU to
    constantly run at 15-20% while the system is awake.  This is almost
    certainly a userspace bug, but since the orientation sensor driver is
    proprietary (ugh), work around this by forcing the driver to sleep after
    each update.
    
    Signed-off-by: Steven Luo <steven+android@steven676.net>

--- drivers/input/misc/uinput.c
+++ drivers/input/misc/uinput.c
@@ -438,8 +438,8 @@ static int uinput_setup_device(struct uinput_device *udev,
 	return retval;
 }
 
-static ssize_t uinput_inject_event(struct uinput_device *udev,
-				   const char __user *buffer, size_t count)
+static inline ssize_t uinput_inject_event(struct uinput_device *udev, 
+	const char __user *buffer, size_t count, __u8 *do_sleep)
 {
 	struct input_event ev;
 
@@ -450,7 +450,10 @@ static ssize_t uinput_inject_event(struct uinput_device *udev,
 		return -EFAULT;
 
 	input_event(udev->dev, ev.type, ev.code, ev.value);
-
+	/* XXX UGLY HACK to throttle system_server orientation sensor code */
+ 	if (ev.type == 2 && ev.code == 2) {
+ 		*do_sleep = 1;
+ 	}
 	return input_event_size();
 }
 
@@ -459,7 +462,7 @@ static ssize_t uinput_write(struct file *file, const char __user *buffer,
 {
 	struct uinput_device *udev = file->private_data;
 	int retval;
-
+    __u8 do_sleep = 0;
 	if (count == 0)
 		return 0;
 
@@ -468,11 +471,15 @@ static ssize_t uinput_write(struct file *file, const char __user *buffer,
 		return retval;
 
 	retval = udev->state == UIST_CREATED ?
-			uinput_inject_event(udev, buffer, count) :
+			uinput_inject_event(udev, buffer, count, &do_sleep) :
 			uinput_setup_device(udev, buffer, count);
 
 	mutex_unlock(&udev->mutex);
-
+/* XXX UGLY HACK to throttle system_server orientation sensor code */
+ 	if (do_sleep) {
+ 		set_current_state(TASK_INTERRUPTIBLE);
+ 		schedule_timeout(HZ/5);
+ 	}
 	return retval;
 }
 
