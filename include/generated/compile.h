8f57b3206bfb84739f31552e7c896263ed117978
LukasAddon <LukasAddon@gmail.com>
2016-05-27 20:36:46 +0300

    Patch Kernel 3.10.9-10

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 23 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#12 SMP PREEMPT Mon Apr 18 22:46:34 EEST 2016"
+#define UTS_VERSION "#23 SMP PREEMPT Fri May 27 16:09:43 EEST 2016"
 #define LINUX_COMPILE_BY "lukas"
 #define LINUX_COMPILE_HOST "lukas-Virtual-Machine"
 #define LINUX_COMPILER "gcc version 5.2.1 20151005 (Linaro GCC 5.2-2015.11-1) "
