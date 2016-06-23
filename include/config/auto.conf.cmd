ca86492bf4ef7ae2e7596a8f4ede0c96845df1ed
LukasAddon <LukasAddon@gmail.com>
2016-06-23 15:18:20 +0300

    patch 3.10.21

--- include/config/auto.conf.cmd
+++ include/config/auto.conf.cmd
@@ -977,7 +977,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "3.10.17"
+ifneq "$(KERNELVERSION)" "3.10.20"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm"
