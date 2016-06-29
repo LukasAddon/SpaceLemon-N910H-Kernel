26443fb1f3b0fa48b6a9f8229e57ad4e89e41a64
LukasAddon <LukasAddon@gmail.com>
2016-06-29 17:24:17 +0300

    Revert "patch 3.10.21"
    This reverts commit ca86492bf4ef7ae2e7596a8f4ede0c96845df1ed.

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -278,8 +278,7 @@ ok:
 	if (task->mm)
 		dumpable = get_dumpable(task->mm);
 	rcu_read_lock();
-	if (dumpable != SUID_DUMP_USER &&
-	    !ptrace_has_cap(__task_cred(task)->user_ns, mode)) {
+	if (!dumpable && !ptrace_has_cap(__task_cred(task), mode)) {
 		rcu_read_unlock();
 		return -EPERM;
 	}
