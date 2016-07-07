e39f41de94506da9e4a7c1bc64d9435700694b17
LukasAddon <LukasAddon@gmail.com>
2016-07-08 00:22:44 +0300

    update ramdisk
    add busybox , update root to 2.76 final

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-export ARCH=arm
-#export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+###############################################################################
+# To all DEV around the world :)                                              #
+# to build this kernel you need to be ROOT and to have bash as script loader  #
+###############################################################################
 
-make exynos5433-tre3calteskt_defconfig
+echo "Clear Folder"
+make clean
+echo "make config"
+make trelte_00_defconfig
+echo "build kernel"
+make -j4
 
-make -j1
+cp -f arch/arm/boot/zImage build_kernel/AIK-Linux/split_img/boot.img-zImage 
+
+build_kernel/AIK-Linux/repackimg.sh
+
+rm -f build_kernel/out/*.zip
+
+cp -f build_kernel/AIK-Linux/image-new.img build_kernel/out/boot.img
+
+cd build_kernel/out/
+
+zip -r SpaceLemon.zip ./
+
+echo "Clear Folder after build kernel"
+make clean
