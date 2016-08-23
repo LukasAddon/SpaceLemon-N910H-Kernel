df0db9b5111e66890c4ec09d121e703b688c70b0
LukasAddon <LukasAddon@gmail.com>
2016-08-23 17:39:59 +0300

    add dynamic Fsync and some fixes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 44 */
+/* This file is auto generated, version 58 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#44 SMP PREEMPT Mon Aug 22 13:17:15 EEST 2016"
+#define UTS_VERSION "#58 SMP PREEMPT Tue Aug 23 17:10:28 EEST 2016"
 #define LINUX_COMPILE_BY "lukas"
 #define LINUX_COMPILE_HOST "lukas-Virtual-Machine"
 #define LINUX_COMPILER "gcc version 5.2.1 20151005 (Linaro GCC 5.2-2015.11-1) "
