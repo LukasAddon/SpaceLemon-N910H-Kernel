b64ca50a7901c6dfb64e4580650fb46fadd13657
LukasAddon <LukasAddon@gmail.com>
2017-08-24 19:36:17 +0300

    Linux-3.10.19

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -2952,6 +2952,7 @@ static void sd_probe_async(void *data, async_cookie_t cookie)
 	msleep(500);
 #endif
 
+	blk_pm_runtime_init(sdp->request_queue, dev);
 	add_disk(gd);
 #ifdef CONFIG_USB_HOST_NOTIFY
 	sdkp->prv_media_present = sdkp->media_present;
@@ -2963,7 +2964,6 @@ static void sd_probe_async(void *data, async_cookie_t cookie)
 
 	sd_printk(KERN_NOTICE, sdkp, "Attached SCSI %sdisk\n",
 		  sdp->removable ? "removable " : "");
-	blk_pm_runtime_init(sdp->request_queue, dev);
 	scsi_autopm_put_device(sdp);
 	put_device(&sdkp->dev);
 #ifdef CONFIG_USB_HOST_NOTIFY
